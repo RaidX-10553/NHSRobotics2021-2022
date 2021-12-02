4e00c877235f38d2dc267b27169047c0c261e289
Antk1ng <74077678+Antk1ng@users.noreply.github.com>
2021-12-02 09:45:37 -0500

    Update PushbotTesting.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PushbotTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PushbotTesting.java
@@ -1,2 +1,33 @@
-// Coders testing pushbot for various things
+// pushbot for various things
+package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+@TeleOp
+public class PushbotTesting extends OpMode {
+    DcMotor frontLeft;
+    DcMotor frontRight;
+    DcMotor backLeft;
+    DcMotor backRight;
+  
+    public void init() {
+       frontLeft = hardwareMap.dcMotor.get("FL");
+       frontRight = hardwareMap.dcMotor.get("FR");
+       backLeft = hardwareMap.dcMotor.get("BL");
+       backRight = hardwareMap.dcMotor.get("BR");
+    }
+
+    public void loop() {
+         float drivePower = -gamepad1.left_stick_y;
+         float rotatePower = gamepad1.right_stick_x;      
+      
+         frontLeft.setPower(drivePower + rotatePower);
+         frontRight.setPower(drivePower - rotatePower);
+         backLeft.setPower(drivePower + rotatePower);
+         backRight.setPower(drivePower - rotatePower);
+    }
+}
