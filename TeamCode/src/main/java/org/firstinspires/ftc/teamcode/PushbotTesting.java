9bdddd1439aa42a37e1b45597f4689783ec0a52f
Antk1ng <74077678+Antk1ng@users.noreply.github.com>
2021-12-03 13:33:23 -0500

    Update PushbotTesting.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PushbotTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PushbotTesting.java
@@ -25,23 +25,44 @@ public class PushbotTesting extends LinearOpMode {
       // If bot spins when going forward, uncomment the stuff below
       // either left or right
       
-    //   frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
-    //   backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+       frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+       backRight.setDirection(DcMotorSimple.Direction.REVERSE);
       
-       frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-       backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+    //   frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+    //   backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
      
     waitForStart();
    
     while (opModeIsActive()) {
-        float drivePower = -gamepad1.left_stick_y;
-        float rotatePower = gamepad1.left_stick_x;      
+        //float drivePower = -gamepad1.left_stick_y;
+        //float rotatePower = gamepad1.left_stick_x;      
       
-        frontLeft.setPower(drivePower + rotatePower);
-        frontRight.setPower(drivePower - rotatePower);
-        backLeft.setPower(drivePower + rotatePower);
-        backRight.setPower(drivePower - rotatePower);
+        //frontLeft.setPower(drivePower + rotatePower);
+        //frontRight.setPower(drivePower - rotatePower);
+        //backLeft.setPower(drivePower + rotatePower);
+        //backRight.setPower(drivePower - rotatePower);
         
+      
+      
+      
+          double y = -gamepad1.left_stick_y; // Remember, this is reversed!
+          double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+          double rx = gamepad1.right_stick_x;
+          
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio, but only when
+            // at least one is out of the range [-1, 1]
+          double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+          double frontLeftPower = (y + x + rx) / denominator;
+          double backLeftPower = (y - x + rx) / denominator;
+          double frontRightPower = (y - x - rx) / denominator;
+          double backRightPower = (y + x - rx) / denominator;
+
+          motorFrontLeft.setPower(frontLeftPower);
+          motorBackLeft.setPower(backLeftPower);
+          motorFrontRight.setPower(frontRightPower);
+          motorBackRight.setPower(backRightPower);
+      
     } 
         idle();
     }
