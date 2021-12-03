18fb65c46fed7db122a1c017a77970a3985a46cf
Antk1ng <74077678+Antk1ng@users.noreply.github.com>
2021-12-03 17:58:54 -0500

    Update PushbotTesting.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PushbotTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PushbotTesting.java
@@ -46,7 +46,7 @@ public class PushbotTesting extends LinearOpMode {
       
       
           double y = gamepad1.left_stick_y; // Remember, this is reversed!
-          double x = -gamepad1.right_stick_x * 1.1; // Counteract imperfect strafing
+          double x = gamepad1.right_stick_x * 1.1; // Counteract imperfect strafing
           double rx = gamepad1.left_stick_x;
           
             // Denominator is the largest motor power (absolute value) or 1
