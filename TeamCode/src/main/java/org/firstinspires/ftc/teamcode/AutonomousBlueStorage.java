6c3cfde7c3ecf45c850bdd76c591ad0dae6ad087
Antk1ng <74077678+Antk1ng@users.noreply.github.com>
2021-12-06 14:22:18 -0500

    Testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousBlueStorage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousBlueStorage.java
@@ -3,3 +3,106 @@
 // Scoring pre-loaded box
 // Going to Carousel and delivering duck onto ground 
 // Parking completely in warehouse
+
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import java.util.Vector;
+
+import java.util.List;
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+
+
+@Autonomous(name="Blue Storage", group="Autonomous")
+
+public class AutonomousBlueStorage extends LinearOpMode{
+
+    private static final String TFOD_MODEL_ASSET = "FreightFrenzy_BCDM.tflite";
+    private static final String[] LABELS = {
+      "Ball",
+      "Cube",
+      "Duck",
+      "Marker"
+    };
+  
+    private static final String VUFORIA_KEY =
+            "AVgDgHH/////AAABmYSjDOxUjkoloTDlPbTfcxMdY+UnPMMGHvIoENz7ljjIJLU7u/WzCXUMDrkDD3rtaVaTTqHY2RiMeeBO0+nWwRe3aHkzxtpSa0LEdicMGhjyk0JyTKusUjg3l0kj1xYOmTidIjIlCc18/Z3FKZTBKEwZrSgakYxiot2r4zBdXcyMekArDle5NCxpDHATu261ZnwhBJc7UKazEkRCbtn9qaN0a5dB0kX3dhGxrargryTg0AuEj17NaXxy8tnq10HEXb2NiwvOJVFiw3YJhEMvyUq5bmY/c0yEchStOyy2bOswp5xtXE5+Qwy8Ty474gYH5ROWRdwrf+6mzFtS4CGdotST1dAOo3uuMgcTNvxsU4CZ";
+
+    private VuforiaLocalizer vuforia;
+
+    private TFObjectDetector tfod;
+
+    @Override
+    public void runOpMode() {
+    
+    //Declare motors and servos here 
+
+
+    @Override
+    public void runOpMode() throws InterruptedException{
+        
+        initVuforia();
+        initTfod();
+
+        if (tfod != null) {
+            tfod.activate();
+
+            tfod.setZoom(2.5, 16.0/9.0);
+        }
+
+        /** Wait for the game to begin */
+        telemetry.addData(">", "Press Play to start OpMode");
+        telemetry.update();
+      
+        waitForStart();
+        
+        if (opModeIsActive()) {
+            while (opModeIsActive()) {
+                if (tfod != null) {
+                    // getUpdatedRecognitions() will return null if no new information is available since
+                    // the last time that call was made.
+                    List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+                    if (updatedRecognitions != null) {
+                      telemetry.addData("# Object Detected", updatedRecognitions.size());
+
+                      // step through the list of recognitions and display boundary info.
+                      int i = 0;
+                      boolean isDuckDetected = false;  
+                      for (Recognition recognition : updatedRecognitions) {
+                        telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
+                        telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
+                                          recognition.getLeft(), recognition.getTop());
+                        telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
+                                recognition.getRight(), recognition.getBottom());
+                        i++;
+                        
+                         // check label to see if the camera now sees a Ball         ** ADDED **
+                        if (recognition.getLabel().equals("Duck")) {            //  ** ADDED **
+                             isDuckDetected = true;                             //  ** ADDED **
+                             telemetry.addData("Object Detected", "Duck");      //  ** ADDED **
+                         } else {                                               //  ** ADDED **
+                             isDuckDetected = false;   
+                             telemetry.addData("Object Not Detected", "Duck");   //  ** ADDED **
+                         }                                                      //  ** ADDED **
+                      }
+                      telemetry.update();
+          
+        
+        if (isStopRequested()) return;
+        
+
+        
+
+
+    }
+}
