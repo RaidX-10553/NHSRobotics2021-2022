f0bf128b54785479a9a1a848ba7f35ee268437c4
Antk1ng <amashkin06@gmail.com>
2021-12-06 15:52:42 -0500

    Current Traj Testing

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -12,19 +12,22 @@ public class MeepMeepTesting {
 
     public static void main(String[] args) {
 
-        MeepMeep meepMeep = new MeepMeep(700, 60);
+        MeepMeep meepMeep = new MeepMeep(1080, 60);
 
-        Pose2d START_POSE = new Pose2d(0, 0, 0);
+        Pose2d START_POSE = new Pose2d(-35, -63.25, Math.toRadians(270));
 
-        Pose2d START_POSE2 = new Pose2d(50,50, 0);
+        Pose2d START_POSE2 = new Pose2d(15,-63.25, Math.toRadians(90));
         RoadRunnerBotEntity bot = new DefaultBotBuilder(meepMeep)
-                .setConstraints(30, 60, Math.toRadians(180), Math.toRadians(180), 13)
+                .setConstraints(30, 30, Math.toRadians(360), Math.toRadians(360), 13.5)
                 .setColorScheme(new ColorSchemeBlueDark())
                 .followTrajectorySequence(drive ->
                         drive.trajectorySequenceBuilder(START_POSE)
                                 //put ur trajectory here
                                 //for example
-                                .splineTo(new Vector2d(10, 10), 15)
+                                .splineTo(new Vector2d(-24.5, -31.2), Math.toRadians(0))
+                                .turn(Math.toRadians(-90))
+                                .splineToLinearHeading(new Pose2d(-63.25, -63.25, Math.toRadians(180)), Math.toRadians(-90))
+                                .strafeRight(28.65)
                                 .build()
                 );
 
@@ -44,8 +47,8 @@ public class MeepMeepTesting {
                 .setBackground(MeepMeep.Background.FIELD_FREIGHTFRENZY_ADI_DARK)
                 .setTheme(new ColorSchemeBlueDark())
                 .setBackgroundAlpha(1f)
-                .addEntity(bot)
-                //.addEntity(bot2) // comment out if you are only using one bot
+                .addEntity(bot.setDimensions(13.5, 13.5))
+                .addEntity(bot2.setDimensions(13.5, 13.5)) // comment out if you are only using one bot
                 .start();
     }
 }
