5b67b97e301edb5fff7d6cbd47094c1c1b1547c3
QU1CK5ILV3R <william.p.clay@gmail.com>
2021-12-17 08:25:04 -0500

    encoder methods and stuff added to encoderbluestorage

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/William/EncoderBlueStorage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/William/EncoderBlueStorage.java
@@ -10,6 +10,7 @@ package org.firstinspires.ftc.teamcode.William;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import org.firstinspires.ftc.teamcode.subsystems.AprilTagLocation;
 import org.firstinspires.ftc.teamcode.subsystems.MarkerDetectionPipeline;
@@ -26,6 +27,17 @@ import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
 @Autonomous(name="EncoderBlueStorage", group="Autonomous")
 public class EncoderBlueStorage extends LinearOpMode {
+    //encoder variable declaration
+    private DcMotor frontLeft = null;
+    private DcMotor frontRight = null;
+    private DcMotor backLeft = null;
+    private DcMotor backRight = null;
+
+    private int frontLeftEncoderPosition;
+    private int frontRightEncoderPosition;
+    private int backLeftEncoderPosition;
+    private int backRightEncoderPosition;
+    //end of encoder variable declaration
 
     /* Declare OpMode members. */
 
@@ -37,6 +49,30 @@ public class EncoderBlueStorage extends LinearOpMode {
 
     @Override
     public void runOpMode() {
+        //initialization for encoders
+        //declare motors
+        frontLeft = hardwareMap.dcMotor.get("FL");
+        frontRight = hardwareMap.dcMotor.get("FR");
+        backLeft = hardwareMap.dcMotor.get("BL");
+        backRight = hardwareMap.dcMotor.get("BR");
+
+        //set encoder values to 0
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        //left motor is reversed so drive wheels don't spin in opposite directions
+        //since the left drive train is opposite
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+        frontLeftEncoderPosition = 0;
+        frontRightEncoderPosition = 0;
+        backLeftEncoderPosition = 0;
+        backRightEncoderPosition = 0;
+        //End of encoder code
         pipeline = new MarkerDetectionPipeline();
 
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
@@ -130,6 +166,38 @@ public class EncoderBlueStorage extends LinearOpMode {
             }
 
        }
+
+    }
+    //encoder method
+    private void drive (int leftTarget, int rightTarget, double speed){
+        frontLeftEncoderPosition += leftTarget;
+        frontRightEncoderPosition += rightTarget;
+        backLeftEncoderPosition += leftTarget;
+        backRightEncoderPosition += rightTarget;
+
+        //sets values
+        frontLeft.setTargetPosition(frontLeftEncoderPosition);
+        frontRight.setTargetPosition(frontRightEncoderPosition);
+        backLeft.setTargetPosition(backLeftEncoderPosition);
+        backRight.setTargetPosition(backRightEncoderPosition);
+
+        //actually tells the motors to run to said position
+        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        //runs the motors at the specified speed until they reach the target position
+        frontLeft.setPower(speed);
+        frontRight.setPower(speed);
+        backLeft.setPower(speed);
+        backRight.setPower(speed);
+
+        //stops other code from running until the target position is reached
+        while(opModeIsActive()&& frontLeft.isBusy()&& frontRight.isBusy() && backLeft.isBusy()&& backRight.isBusy())
+        {
+            idle();
+        }
     }
 }
 
