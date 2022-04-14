4da0590b372aeaa33d9eaf7f5fedf815b671771e
fbkh2429 <enea07vogli@gmail.com>
2022-04-14 02:15:31 -0400

    started x drive project

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ProjectX_Enea.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ProjectX_Enea.java
@@ -1,4 +1,30 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+@TeleOp
+
 public class ProjectX_Enea {
+    public ElapsedTime runtime = new ElapsedTime();
+    public DcMotor Front = null;
+    public DcMotor Left = null;
+    public DcMotor Right = null;
+    public DcMotor Back = null;
+    public HardwareMap hardwareMap = null;
+
+    @Override
+    public void init(hardwareMap) {
+        Telemetry.addData("Status", "Initialized");
+        Front = hardwareMap.dcMotor.get("FL");
+        Right = hardwareMap.dcMotor.get("FR");
+        Left = hardwareMap.dcMotor.get("BL");
+        Back = hardwareMap.dcMotor.get("BR");
+        Right.setDirection(DcMotor.Direction.REVERSE);
+        Back.setDirection(DcMotor.Direction.REVERSE);
+    }
 }
